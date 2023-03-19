b0881e0ad4ee6413e297e5d80823e3bdb5757feb
lorlay22 <84510863+lorlay22@users.noreply.github.com>
2023-03-18 19:28:21 -1100

    v1

--- src/main/java/Modele/Charactere.java
+++ src/main/java/Modele/Charactere.java
@@ -1,4 +1,17 @@
 package Modele;
 
 public abstract class Charactere {
+    //attributs de tous les personnages
+    public String name;
+    public int maxHp, hp, xp;
+    //construction pour personnage
+    public Charactere( String name, int maxHp, int x){
+        this.name=name;
+        this.maxHp=maxHp;
+        this.xp = xp;
+        this.hp=maxHp;
+    }
+    //methode des personnages
+    public abstract int attack();
+    public abstract int defend();
 }
--- src/main/java/Modele/Wizard.java
+++ src/main/java/Modele/Wizard.java
@@ -1,4 +1,55 @@
 package Modele;
 
-public class Wizard {
-}
+public class Wizard extends Charactere {
+    //integers pour les points des offensives deffensives
+    public int numOffensive, numDefensive;
+    // diiferentes attaques et offensives
+    public String[]defensive={"Strength", "Power","Might","Godlike Strength"};
+    public String[] offensive={ "Heavy Bones", "Stoneskin","Scale Armor", "Holy Aura"};
+    // specific constructor du wizard
+    public Wizard (String name) {
+        //calling constructor of superclass
+
+        super(name, 100, 0);
+        this.numOffensive=0;
+        this.numDefensive=0;
+        //le wizard choisit un trait quand il est créé
+        chooseTrait();
+    }
+    // les methodes specifiques du wizard
+
+    @Override
+    public int attack() {
+        return 0;
+    }
+
+    @Override
+    public int defend() {
+        return 0;
+    }
+    // wizard choisit un trait pour chaque skill path
+    public void chooseTrait() {
+        //pas sure de ces 2 lignes
+        Wizard.clearConsole();
+        Wizard.printHeading("Choose a trait");
+        System.out.println("(1)" + offensive[numOffensive]);
+        System.out.println("(2)" + defensive[numDefensive]);
+        //wizard fait son choix
+        int input = Wizard.readInt("->", 2);
+        Wizard.clearConsole();
+        //actions selon choix
+        if (input == 1) {
+            Wizard.printHeading(" You chose" + offensive[numOffensive] +);
+            numOffensive++;
+        } else {
+            Wizard.printHeading(" You chose" + defensive[numDefensive] +);
+            numDefensive++;
+        }
+        Wizard.anythingToContinue();
+    }
+
+
+    }
+
+
+
