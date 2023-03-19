e140b08f4c54e5c77d2000930c21760688237f70
lorlay22 <84510863+lorlay22@users.noreply.github.com>
2023-03-18 22:01:17 -1100

    v1

--- src/main/java/Modele/SortingHat.java
+++ src/main/java/Modele/SortingHat.java
@@ -1,7 +1,25 @@
 package Modele;
+import java.util.Random;
 
 public class SortingHat {
-    public String assignHouse(Wizard wizard) {
-        return null;
+    public House assignRandomHouse(){
+        Random rand = new Random();
+        int randomHouse = rand.nextInt(4)+1;
+
+        switch (randomHouse){
+            case 1 :
+                return new House("Gryffondor");
+            case 2 :
+                return new House("Slytherin");
+            case 3 :
+                return new House("Ravenclaw");
+            case 4 :
+                return new House("Hufflepuff");
+            default:
+                return null;
+        }
+        SortingHat hat = new SortingHat();
+        House house = hat.assignRandomHouse();
+        System.out.println("Your house is" + house.getName()+ "house");
     }
 }
